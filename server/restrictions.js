34cbb5c82442dae67cb790787fb75f386cdfd972
jacob-finley <jacobfinley@unomaha.edu>
2024-03-13 16:15:13 -0500

    Did some modifications to various things. 1. removed React pngs from "client"
    2. added node_modules to be able to run csv-parse
    
    3. Did some experimenting displaying the csvdata
    
    4. added a temporary sql script file with classroom data

--- server/restrictions.js
+++ server/restrictions.js
@@ -1,18 +1,29 @@
-function matchRooms() {
-    console.log("I AM ALIVE");
-}
-
 var fs = require("fs");
 var { parse } = require("csv-parse");
-
 var csvData = [];
 
-fs.createReadStream('./server/uploads/test.csv')
-    .pipe(parse({ delimiter: ',', from_line: 2 }))
+function matchRooms() {
+    console.log("I AM ALIVE");
+    console.log(csvData);
+    console.log(csvData.length);
+}
+
+function readCSVData(callback) {
+    fs.createReadStream('./server/uploads/test.csv')
+    .pipe(parse({from_line: 2}))
     .on('data', function (row) {
         // console.log(row);
         csvData.push(row);
+    })
+    .on('end', function() {
+        callback();
     });
+}
 
-matchRooms();
-console.log(csvData);
+function main() {
+    readCSVData(function() {
+        matchRooms();
+    });
+}
+
+main();
---
+++ server/uploads/classroom_details.sql
@@ -0,0 +1,29 @@
+INSERT INTO RoomsList
+    (
+    Room_Number, Displays, Connectivity_Info, Seats, Computers
+    )
+VALUES
+    ('108', 'Three 80" TVs', 'Shared IS&T/CoE classroom - Video Conferencing - HDMI, Mini DisplayPort', 12, 1),
+    ('150', 'Two 80" TVs', 'Shared IS&T/CoE classroom/conference room - HDMI', 16, 1),
+    ('153', 'Two Projectors', 'Shared IS&T/CoE classroom - HDMI, Mini DisplayPort', 42, 1),
+    ('155', 'Projector', 'Shared IS&T/CoE classroom - HDMI, lapel microphone', 40, 1),
+    ('157', 'Projector', 'Shared IS&T/CoE classroom - HDMI', 24, 1),
+    ('160', 'Projector, Four 70" TVs', 'Shared IS&T/CoE classroom - HDMI, Mini DisplayPort, VGA, lapel microphone', 42, 1),
+    ('161', 'Projector, Three TVs', 'Shared IS&T/CoE classroom - HDMI, lapel microphone', 30, 1),
+    ('164', 'Projector, Four 70" TVs', 'Shared IS&T/CoE classroom - HDMI, Mini DisplayPort, VGA, lapel microphone', 56, 1),
+    ('250', 'Two 80" TVs', 'Shared IS&T/CoE classroom/conference room - HDMI', 16, 1),
+    ('252', 'Two Projectors', 'Shared IS&T/CoE classroom - HDMI, lapel microphone', 58, 1),
+    ('256', 'Projector', 'Shared IS&T/CoE classroom - HDMI, USB-C', 40, 1),
+    ('259', 'Projector', 'Shared IS&T/CoE classroom - HDMI, Mini DisplayPort', 16, 1),
+    ('260', 'Projector', 'Shared IS&T/CoE classroom - HDMI, USB-C', 60, 60),
+    ('261', 'Projector', 'Shared IS&T/CoE classroom - HDMI, USB-C', 24, 1),
+    ('263', 'Two Projectors', 'Shared IS&T/CoE classroom - HDMI, lapel microphone', 50, 1),
+    ('269', 'Projector', 'IS&T classroom with HDMI, USB-C', 30, 1),
+    ('270', 'Projector', 'Shared IS&T/CoE classroom - HDMI', 16, 1),
+    ('274', 'Projector', 'Shared IS&T/CoE classroom - HDMI, USB-C', 32, 1),
+    ('276', 'Projector', 'IS&T classroom - HDMI, USB-C', 40, 35),
+    ('278', 'Projector', 'IS&T classroom - HDMI, USB-C', 40, 35),
+    ('279', 'Three Projectors', 'IS&T Collaboration & Hosting Center - HDMI, USB Type-C, DisplayPort, Thunderbolt, Lightning (Apple)', 30, 1),
+    ('350', 'Two 80" TVs, One Projector', 'Shared IS&T/CoE classroom/conference room - HDMI, VGA', 16, 1),
+    ('361', 'Two Projectors, 86" TV', 'IS&T classroom - HDMI, USB-C', 40, 40),
+    ('391A', '55" TV', 'IS&T classroom/conference room - HDMI', 10, 1);
