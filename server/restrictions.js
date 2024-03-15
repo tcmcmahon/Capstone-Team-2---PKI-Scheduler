5be31c6f1aa337b6c31ef1bc5a706b05b58b26e2
jacob-finley <jacobfinley@unomaha.edu>
2024-03-15 18:23:52 -0500

    Updated restrictions.js so now that the data is being saved properly. Can now start to preprocess the data

--- server/restrictions.js
+++ server/restrictions.js
@@ -3,90 +3,92 @@ import ClassDescription from './Class_Descriptions.js';
 import fs from 'fs';
 import { parse } from 'csv-parse';
 
+
 /* global variables */
-var csvData = [];
-var classData = []; // will hold instances of classDescription
+var classData = []; // will hold instances of classDescription, will end up with the data for all of the classes
+
 
 /* read data from the csv file */
 function readCSVData() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
+        var prevClassName; // holds the previous class stated in csv file
         fs.createReadStream('./server/uploads/test.csv')
-        .pipe(parse({from_line: 4}))
-        .on('data', function (row) {
-            csvData.push(row);
+        .pipe(
+            parse({from_line: 4}) // starts reading at line 4 with "AREN 1030 - DESIGN AND SIMULATION STUDIO I"
+        ) 
+        .on('data', function (row) { // iterates through each row in csv file
+            var cd = new ClassDescription(); // creates object to store class data
+            if (row[0] === '') { // will create a new object for classData
+                // TODO: all of these are not needed, will need to reduce data
+                cd.name = prevClassName;
+                cd.term = row[1];
+                cd.termCode = row[2];
+                cd.deptCode = row[3];
+                cd.subjCode = row[4];
+                cd.catalogNumber = row[5];
+                cd.course = row[6];
+                cd.sectionNumber = row[7];
+                cd.courseTitle = row[8];
+                cd.sectionType = row[9];
+                cd.topic = row[10];
+                cd.meetingPattern = row[11];
+                cd.meeting = row[12];
+                cd.instructor = row[13];
+                cd.room = row[14];
+                cd.status = row[15];
+                cd.session = row[16];
+                cd.campus = row[17];
+                cd.instMethod = row[18];
+                cd.integPattern = row[19];
+                cd.schedulePrint = row[20];
+                cd.consent = row[21];
+                cd.creditHrsMin = row[22];
+                cd.creditHrs = row[23];
+                cd.gradeMode = row[24];
+                cd.attributes = row[25];
+                cd.courseAttributes = row[26];
+                cd.roomAttributes = row[27];
+                cd.enrolled = row[28];
+                cd.maximumEnrollments = row[29];
+                cd.priorEnrollments = row[30];
+                cd.projectedEnrollments = row[31];
+                cd.waitCap = row[32];
+                cd.rmCapRequest = row[33];
+                cd.crossListings = row[34];
+                cd.crossListMaximum = row[35];
+                cd.crossListProjected = row[36];
+                cd.crossListWaitCap = row[37];
+                cd.crossListCapRequest = row[38];
+                cd.linkTo = row[39];
+                cd.comments = row[40];
+                cd.notes1 = row[41];
+                cd.notes2 = row[42];
+                classData.push(cd);
+                // no need to delete cd since javascript already performs garbage collection
+            }
+            else { // will save the previous class name for the next row
+                prevClassName = row[0];
+            } // end of if statement
         })
         .on('end', function() {
-            var cd = new ClassDescription();
-            var rowCount = 0;
-            var prevClassName;
-            for (var i = 0; i < csvData.length; i++) {
-                if (csvData[i][0] === '') {
-                    cd.name = prevClassName;
-                }
-                else {
-                    prevClassName = csvData[i][0];
-                    continue;
-                }
-                cd.term = csvData[i][1];
-                cd.termCode = csvData[i][2];
-                cd.deptCode = csvData[i][3];
-                cd.subjCode = csvData[i][4];
-                cd.catalogNumber = csvData[i][5];
-                cd.course = csvData[i][6];
-                cd.sectionNumber = csvData[i][7];
-                cd.courseTitle = csvData[i][8];
-                cd.sectionType = csvData[i][9];
-                cd.topic = csvData[i][10];
-                cd.meetingPattern = csvData[i][11];
-                cd.meeting = csvData[i][12];
-                cd.instructor = csvData[i][13];
-                cd.room = csvData[i][14];
-                cd.status = csvData[i][15];
-                cd.session = csvData[i][16];
-                cd.campus = csvData[i][17];
-                cd.instMethod = csvData[i][18];
-                cd.integPattern = csvData[i][19];
-                cd.schedulePrint = csvData[i][20];
-                cd.consent = csvData[i][21];
-                cd.creditHrsMin = csvData[i][22];
-                cd.creditHrs = csvData[i][23];
-                cd.gradeMode = csvData[i][24];
-                cd.attributes = csvData[i][25];
-                cd.courseAttributes = csvData[i][26];
-                cd.roomAttributes = csvData[i][27];
-                cd.enrolled = csvData[i][28];
-                cd.maximumEnrollments = csvData[i][29];
-                cd.priorEnrollments = csvData[i][30];
-                cd.projectedEnrollments = csvData[i][31];
-                cd.waitCap = csvData[i][32];
-                cd.rmCapRequest = csvData[i][33];
-                cd.crossListings = csvData[i][34];
-                cd.crossListMaximum = csvData[i][35];
-                cd.crossListProjected = csvData[i][36];
-                cd.crossListWaitCap = csvData[i][37];
-                cd.crossListCapRequest = csvData[i][38];
-                cd.linkTo = csvData[i][39];
-                cd.comments = csvData[i][40];
-                cd.notes1 = csvData[i][41];
-                cd.notes2 = csvData[i][42];
-                console.log(cd);
-                var copy = cd;
-                classData.push(copy);
-                cd.clearDescription();
-                rowCount++;
-            } // end of for loop
-            console.log('Count: ' + rowCount);
-            resolve(classData);
+            resolve(classData); // saves the data for classData
         }) // end of fs read
     }); // end of return
 } // end of readCSVData
 
 
-/* main function */
+/* verify that the data is saved */
+function main2ElectricBoogaloo() {
+    console.log(classData[308]);
+}
+
+
+/* main function, is async because fs.createReadStream() */
 async function main() {
     await readCSVData();
-    console.log(classData); // FIXME: data is null when it should be the same as before
+    console.log(classData[0]); 
     console.log(classData.length);
+    main2ElectricBoogaloo();
 } // end of main
 
 
