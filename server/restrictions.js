1cc73529556e96e8b42d81e28eec987537b5c401
jacob-finley <jacobfinley@unomaha.edu>
2024-03-15 23:37:28 -0500

    Might have finished the functionality that pushes the cross listed courses to the array. Now need to be able to check if a class is cross listed and pass it

--- server/restrictions.js
+++ server/restrictions.js
@@ -2,12 +2,36 @@
 import ClassDescription from './Class_Descriptions.js';
 import fs from 'fs';
 import { parse } from 'csv-parse';
-import rooms from './uploads/rooms.json' assert {type: 'json'};
+// import rooms from './uploads/rooms.json' assert {type: 'json'};
 
 
 /* global variables */
 var classData = []; // will hold instances of classDescription, will end up with the data for all of the classes
 var crossListedCoursesToCheck = []; // will temporarily hold classes that are cross listed and skip them if listed
+
+
+/* split cross listed courses and add them to global array */
+function pushCrossListedCourses(crossListings) {
+    var courses;
+    var sectionNumber;
+    // remove either See or Also when listed
+    crossListings = (crossListings.includes("See")) ? crossListings.slice(4) : crossListings.slice(5);
+    // split if multiple cross listed courses
+    courses = crossListings.split(", ");
+    // go through each course and split
+    for (var i = 0; i < courses.length; i++) {
+        if (crossListedCoursesToCheck.includes(courses[i])) {
+            continue;
+        }
+        sectionNumber = (courses[i].slice(-3).includes('00')) ? courses[i].slice(-1) : courses[i].slice(-3);
+        crossListedCoursesToCheck.push([courses[i].slice(0,-4),sectionNumber]); // [course name, section number]
+    }
+}
+
+
+function checkIfCrossListed(courseName) {
+    // TODO: Add functionality here
+}
 
 
 /* read data from the csv file */
@@ -20,34 +44,26 @@ function readCSVData() {
         ) 
         .on('data', function (row) { // iterates through each row in csv file
             var cd = new ClassDescription(); // creates object to store class data
+            // if (row[18] === 'Distance Education') { } // ASK : are classes that are labeled Distance Education fully remote?
             if (row[0] === '') { // will create a new object for classData
-                // TODO: all of these are not needed, will need to reduce data
                 cd.name = prevClassName;
                 cd.sectionNumber = row[7];
-                cd.sectionType = row[9]; // ASK : not all labs that are on the csv are listed in the website
+                cd.isLab = (row[9] === "Laboratory") ? true : false;
                 cd.meetingPattern = row[11];
-                cd.room = row[14];
                 cd.session = row[16];
-                cd.campus = row[17];
-                cd.maximumEnrollments = row[29];
-                cd.crossListings = row[34];
-                cd.crossListMaximum = row[35]; // ASK : Is the crossListMaximum consist of the total number of people who can take the course from all cross listed courses
+                cd.campus = (row[17] == "UNO-IS") ? "IS&T" : "CoE";
+                if (row[34] === '') { // isn't cross listed
+                    cd.maximumEnrollments = row[29];
+                }
+                else { // is cross listed
+                    cd.maximumEnrollments = row[35]; // ASK : Is the crossListMaximum consist of the total number of people who can take the course from all cross listed courses
+                    pushCrossListedCourses(row[34]);
+                }
                 classData.push(cd);
-                // no need to delete cd since javascript already performs garbage collection
             }
             else { // will save the previous class name for the next row
                 prevClassName = row[0];
             } // end of if statement
-            var isRoom = false;
-            for (var room in rooms) {
-                if (String(cd.room).includes(room) || cd.room === null) {
-                    isRoom = true;
-                    break;
-                }
-            }
-            if (isRoom == false) {
-                console.log("Room (" + cd.room + ") not listed");
-            }
         })
         .on('end', function() {
             resolve(classData); // saves the data for classData
@@ -58,16 +74,10 @@ function readCSVData() {
 
 /* verify that the data is saved */
 function main2ElectricBoogaloo() {
-    // console.log(classData[308]);
-    for (var room in rooms) {
-        console.log("Room: " + room);
-        console.log("Type: " + rooms[room].RoomType);
-        console.log("Info: " + rooms[room].Info);
-        console.log("Conf: " + rooms[room].Conferencing);
-        console.log("Conn: " + rooms[room].Connectivity);
-        console.log("Seat: " + rooms[room].Seats);
-        console.log("Comp: " + rooms[room].Computers + "\n");
-    }
+    // console.log(classData[74]);
+    console.log(crossListedCoursesToCheck);
+    console.log(crossListedCoursesToCheck.length);
+    console.log(classData.length);
 }
 
 
@@ -76,7 +86,7 @@ async function main() {
     await readCSVData();
     // console.log(classData[0]); 
     // console.log(classData.length);
-    // main2ElectricBoogaloo();
+    main2ElectricBoogaloo();
 } // end of main
 
 
