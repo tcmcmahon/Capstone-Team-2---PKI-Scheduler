81ef7b091aff13315337da036d0ef75e7326fddb
Abdoullatoundji <Abdoullatoundji@unomaha.edu>
2024-03-26 22:52:52 -0500

    Change Goto Calendar link into a button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link component for navigation
 import img from './O-UNO_Type_Color_White.png';
 import img2 from './photo-1606761568499-6d2451b23c66.avif';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 
 function App() {
   const [file, setFile] = useState(null);
-  const [classData, setClassData] = useState([]); // State to store class data array
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -28,10 +27,6 @@ function App() {
 
       if (response.ok) {
         alert('File uploaded successfully!');
-        // Fetch class data array from server after successful upload
-        const dataResponse = await fetch('http://localhost:3000/classData');
-        const data = await dataResponse.json();
-        setClassData(data);
       } else {
         throw new Error('Failed to upload file.');
       }
@@ -48,9 +43,10 @@ function App() {
       <h3 style={{textAlign: "center", margin: "auto"}}>Upload a CSV File below to get started</h3>
       <p style={{margin: "auto", textAlign: "center", backgroundColor: "black", color: "white", width: "25%", padding: "10px"}}>
         <input type="file" accept=".csv" onChange={handleFileChange} />
-        <button onClick={handleUpload}>Upload</button>
-        {/* Add Link to navigate to calendar page */}
-        <Link to="/calendar">Go to Calendar</Link>
+        <button onClick={handleUpload} style={{marginRight: '50px'}}>Upload</button>
+        <Link to="/calendar" style={{ textDecoration: 'none' }}>
+          <button style={{marginRight: '50px'}}>Go to Calendar</button>
+        </Link>
       </p>
       <body style={{backgroundImage: `url(${img2})`, backgroundSize: "cover", height: "100vh", width: "90%", display: "block", margin: "auto"}}></body>
     </div>
@@ -58,3 +54,4 @@ function App() {
 }
 
 export default App;
+
