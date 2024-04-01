a9757bee528aacabc522cbb9e9ec2bc85470c093
tcmcmahon <106412372+tcmcmahon@users.noreply.github.com>
2024-04-01 18:24:35 -0500

    Used JFinley's more elegant algo
    Removed my convoluted attempt

--- server/restrictions.js
+++ server/restrictions.js
@@ -36,136 +36,36 @@ ex.listen(3001, () => console.log("Server is up"));
 var nonFinal = [];
 
 function algoAssign()
-{   let k = 0;//room counter
-    let u = [];//startTimes-endTimes
-    let o = [];
-
-    /*Assign classes a room, first pass through*/
-    for(let i = 0; i < classData.length; i++)
+{    
+    var hash = {};
+    var day = 'MW'; // 'M' or 'T' or 'W' or 'R' or 'F' or a combination
+    var total_classes = 0;
+    for (var _class in classData) 
     {
-        let y = [];//stores meeting info
-
-        y = classData[i].meetingDates;//store meeting info
-        u = y[0].startTime + "-" + y[0].endTime;//store startTimes-endTimes
-        
-        if(k == 39)//if room number is greater than 39 reset to 0
+        var sesList = classData[_class].meetingDates
+        for (var ses in sesList) 
         {
-            k = 0;
-            //push room with info
-            nonFinal[i] = ({room: Object.keys(rooms)[k], class: classData[i].name + " Section " + classData[i].sectionNumber, time: u});  
+            if (!sesList[ses].days.includes(day)) 
+            {
+                continue;
+            }
+            
+            // var splicedTime = ${sesList[ses].startTime}-${sesList[ses].endTime};
+            var splicedTime = "" + sesList[ses].startTime + "";
+            if (splicedTime in hash) 
+            {
+                hash[splicedTime]++;
+            }
+            else 
+            {
+                hash[splicedTime] = 1;
+            }
+            total_classes++;
         }
-        else
-        {
-            //push room with info
-            nonFinal[i] = ({room: Object.keys(rooms)[k], class: classData[i].name + " Section " + classData[i].sectionNumber, time: u});
-        }
-        k++;//increment room counter
-        }   
-        /*Check for duplicate timeslots and assign to array l*/
-        let l = [];
-        for(let j = 0; j < nonFinal.length; j++)
-        {
-            let p = 1;//counter for next class in each pair of classes 
-            for(let g = 0; g <= nonFinal.length && (j + p) < 295; g++)
-            {
-                if(nonFinal[j].room == nonFinal[j+p].room && nonFinal[j].time == nonFinal[j+p].time)//if room/time and next room/time are the same
-                {
-                    if(nonFinal[j].class.includes("CONE") || nonFinal[j].class.includes("AREN"))//if class name contains unnassignable class
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        l.push(nonFinal[j]);//push room to array l, will store duplicate timeslots
-                    }
-                }
-                p++;//increment counter for next class
-            }
-        }
-        /*Get unique duplicate timeslots*/
-        l = [... new Set(l)];
-
-        /*Get rooms that are available*/
-        let q = [];
-        let f = 0;//counter for classes with duplicate timeslots
-        for(let p = 0; p < Object.keys(rooms).length; p++)//loop for all rooms
-        {
-            for(let h = 0; h < nonFinal.length; h++)//loop for all first class assignments
-            {
-                if(nonFinal[h].room == Object.keys(rooms)[p] && nonFinal[h].room == l[f].room && nonFinal[h].time != l[f].time)//if class room is valid and class room is in list of duplicates and time is not 
-                {
-                    q.push(nonFinal[h].room);//push class to array q
-                    if(f > 80)//if we are above the length of the duplicates array
-                    {
-                        break;
-                    }
-                    f++;//increment duplicate timeslot counter
-                }
-            }
-        }
-        
-        /*Check if rooms are same, and if so move to next room up*/
-        let m = 0;//counter for available classes
-        for(let b = 0; b < l.length && b + 1 < l.length; b++)//if b is less than number of duplicates and b + 1 is also
-        {
-            let x = 1;//counter for next class
-            if(m == 19)//if we are at the end of available rooms
-            {
-                m = 0;
-            }
-            for(let z = 0; z < l.length && m < 19 && m + 1 <= 19 && b + x < 52; z++)//while we have more rooms that need to be assigned
-            {
-                if(x == 52)//reset next class counter
-                {
-                    x = 0;
-                }
-                else
-                {
-                    if(l[b].room == l[b+x].room)//if room is the same as the next room, assign to the next room in the list of available rooms
-                    {
-                        l[b].room = q[m+1];
-                    }
-                    else//else assign current room in list of rooms
-                    {
-                        l[b].room = q[m];
-                    }
-                }
-                x++;//increment next class counter
-            }
-            m++;//increment available room counter
-        }
-        
-        /*If room and time are the same replace with new room from previous loop*/
-        let d = 0;
-        //loop and re assign to the first array with the new values
-        for(let n = 0; n < nonFinal.length; n++)
-        {
-            if(d == 52)
-            {
-                d = 0;
-            }
-            if(nonFinal[n].room == l[d].room && nonFinal[n].time == l[d].time)
-            {
-                nonFinal[n] = l[d];
-            }
-            d++;
-            if((!(nonFinal[n].class.includes("CONE")) || !(nonFinal[n].class.includes("AREN"))) && nonFinal[n].room == "149")
-            {
-                console.log(nonFinal[n]);
-            }
-        }
-        /*Output all classes by room*/
-        /*for(let s = 0; s < Object.keys(rooms).length; s++)
-        {
-            for(let a = 0; a < nonFinal.length; a++)
-            {
-                if(nonFinal[a].room == Object.keys(rooms)[s])
-                {
-                    console.log(nonFinal[a]);
-                }
-            }
-        }*/
     }
+    console.log(hash);
+    console.log(total_classes);
+}
 
 /* global variables */
 var classData = []; // will hold instances of classDescription, will end up with the data for all of the classes
