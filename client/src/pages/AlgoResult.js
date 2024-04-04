cb02c8ae9caf4bc235fad30be4e1ae9ed0993244
Travis McMahon <travis.mcmahon@ssandtech.com>
2024-04-04 15:49:41 -0500

    Changed output page to look better

--- client/src/pages/AlgoResult.js
+++ client/src/pages/AlgoResult.js
@@ -12,10 +12,10 @@ algoResult.push(JSON.stringify(result, null, 2));
 
 export default function AlgoResult(){
     return <div>
-               <body style={{backgroundImage: `url(${img2})`, backgroundRepeat: "repeat-y", backgroundPosition: "top"}}>
+               <body style={{backgroundImage: `url(${img2})`, backgroundSize: "contain", backgroundRepeat: "repeat-y", bacbackgroundPosition: "top"}}>
                 <h1 style={{backgroundImage: `url(${img})`, backgroundSize: "cover", height: "110px", backgroundColor: "black"}}></h1>
                 <h2 style={{backgroundColor: "black", color: "white", width: "15%", margin: "auto", textAlign: "center"}}>Algorithm Results</h2>
-                <h3 style={{textAlign: "center", margin: "auto", backgroundColor: "black", color: "white", width: "70%"}}><pre>{algoResult}</pre></h3>
+                <h3 style={{textAlign: "left", margin: "auto", backgroundColor: "black", color: "white", width: "70%"}}><pre>{algoResult}</pre></h3>
                 </body>
             </div>
 }
