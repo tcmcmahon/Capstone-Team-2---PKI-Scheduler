26dcb34042bfb3fd464c0e33b28ea2fa8581dd0e
Travis McMahon <travis.mcmahon@ssandtech.com>
2024-04-05 09:00:44 -0500

    Fixed calendar date locking

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -8,7 +8,8 @@ import {
   Appointments,
   AppointmentTooltip,
   ViewSwitcher,
-  Toolbar
+  Toolbar,
+  DateNavigator
 } from '@devexpress/dx-react-scheduler-material-ui';
 import img from '../resources/O-UNO_Type_Color_White.png';
 import axios from 'axios';
@@ -32,12 +33,13 @@ export default function Calendar(){
       <h2 style={{textAlign: "center", margin: "auto", backgroundColor: "black", color: "white", width: "23%"}}>Class schedule for the week</h2>
   <Paper>
     <Scheduler data={cData}>
-    <ViewState currentDate={currentDate} />
+    <ViewState defaultCurrentDate={currentDate} />
     <WeekView startDayHour={8} endDayHour={20} excludedDays={[0,6]} />
     <DayView startDayHour={8} endDayHour={20} excludedDays={[0,6]} />
     <Appointments />
     <AppointmentTooltip/>
     <Toolbar/>
+    <DateNavigator/>
     <ViewSwitcher/>
     </Scheduler>
   </Paper>
