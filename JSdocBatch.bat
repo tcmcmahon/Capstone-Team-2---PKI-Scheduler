877f6b4e2f6984c85b3e0cac9c724ebfbb40ccc4
Travis McMahon <travis.mcmahon@ssandtech.com>
2024-04-09 07:46:12 -0500

    Batch file for JSdoc creation

---
+++ JSdocBatch.bat
@@ -0,0 +1,1 @@
+jsdoc ./client/src/App.js ./client/src/Navbar.js ./client/src/index.js ./client/src/pages/Upload.js ./client/src/pages/AlgoResult.js ./client/src/pages/Calendar.js ./client/src/pages/Home.js ./server/restrictions.js ./server/Class_Objects.js ./README.md ./docs/index.html -d ./docs
