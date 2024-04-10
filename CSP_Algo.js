c1fd0b98fe53160724b5bb8df037cb51044cd45a
Jshadbolt001 <70242314+Jshadbolt001@users.noreply.github.com>
2024-04-10 16:04:33 -0500

    Added arc consistency to CSP
    Added arc consistency to the CSP assignment. Might be some bugs I haven't found yet, but if this does work completely it's the last performance piece before I can start implementing it into the actual server

--- CSP_Algo.js
+++ CSP_Algo.js
@@ -1,9 +1,17 @@
 class Course {
-    constructor(name, timeslots, possibleRooms){
+    constructor(name, timeslots, possibleRooms) {
         this.name = name
         this.timeslots = timeslots
         this.neighbors = []
         this.possibleRooms = possibleRooms
+    }
+}
+
+class CourseAssignCSP {
+    constructor(courses, allRooms, arcs) {
+        this.courses = courses
+        this.arcs = arcs
+        this.allRooms = allRooms
     }
 }
 
@@ -15,39 +23,48 @@ CourseSA = new Course("SA", [], allRooms)
 CourseQLI = new Course('QLI', [], allRooms)
 CourseNSW = new Course('NSW', [], allRooms)
 CourseVIC = new Course('VIC', [], allRooms)
-CourseT = new Course('T', [], allRooms)
+CourseT = new Course('T', [], [102])
 
 CourseWA.neighbors = [CourseNT, CourseSA]
 CourseNT.neighbors = [CourseWA, CourseSA, CourseQLI]
-CourseSA.neighbors =  [CourseWA, CourseNT,  CourseQLI, CourseNSW, CourseVIC]
+CourseSA.neighbors = [CourseWA, CourseNT, CourseQLI, CourseNSW, CourseVIC]
 CourseQLI.neighbors = [CourseNT, CourseSA, CourseNSW]
 CourseNSW.neighbors = [CourseSA, CourseQLI, CourseVIC]
 CourseVIC.neighbors = [CourseSA, CourseNSW]
 
-CSPProblem = [CourseWA, CourseNT, CourseSA, CourseQLI, CourseNSW, CourseVIC, CourseT]
+CSPProblem_Courses = [CourseWA, CourseNT, CourseSA, CourseQLI, CourseNSW, CourseVIC, CourseT]
+
+CSPProblem = new CourseAssignCSP(CSPProblem_Courses, allRooms)
+
+//Hardcoding these before I can simply get them from checking the entered courses
+CSPProblem.arcs = [[CourseWA, CourseNT], [CourseWA, CourseSA], [CourseNT, CourseSA], [CourseNT, CourseQLI],
+[CourseSA, CourseQLI], [CourseSA, CourseNSW], [CourseSA, CourseVIC],
+[CourseQLI, CourseNSW], [CourseNSW, CourseVIC]]
 
 //Assignment functions begin here
 
-function backtrackingSearch(csp){
+function backtrackingSearch(csp) {
     return recursiveBacktrackingSearch({}, csp)
 }
 
-function recursiveBacktrackingSearch(assignment, csp){
-    if (isComplete(assignment, csp)){
+function recursiveBacktrackingSearch(assignment, csp) {
+    if (isComplete(assignment, csp)) {
         return assignment
     }
 
     const selectedCourse = selectCourseMRV(assignment, csp)
     const orderedRooms = orderDomainValues(selectedCourse, selectedCourse.possibleRooms, assignment, csp)
 
-    for (let room in orderedRooms){
+    for (let room in orderedRooms) {
 
-        if (isConsistent(selectedCourse, selectedCourse.possibleRooms[room], assignment, csp)){
+        if (isConsistent(selectedCourse, selectedCourse.possibleRooms[room], assignment, csp)) {
             assignment[selectedCourse.name] = selectedCourse.possibleRooms[room]
-            let result = recursiveBacktrackingSearch(assignment, csp)
 
-            if (result !== null){
-                return result
+            if (arc_consistency(assignment, csp)) {
+                let result = recursiveBacktrackingSearch(assignment, csp)
+                if (result !== null) {
+                    return result
+                }
             }
 
             delete assignment[selectedCourse]
@@ -57,13 +74,13 @@ function recursiveBacktrackingSearch(assignment, csp){
     return null
 }
 
-function isComplete(assignment, csp){
+function isComplete(assignment, csp) {
     console.log(assignment)
-    return Object.keys(assignment).length === csp.length
+    return Object.keys(assignment).length === csp.courses.length
 }
 
-function selectCourseMRV(assignment, csp){
-    const unassignedCourses = csp.filter(course => !(course.name in assignment))
+function selectCourseMRV(assignment, csp) {
+    const unassignedCourses = csp.courses.filter(course => !(course.name in assignment))
 
     return unassignedCourses.reduce((minCourse, course) => {
         return course.possibleRooms.length < minCourse.possibleRooms.length ? course : minCourse
@@ -71,18 +88,18 @@ function selectCourseMRV(assignment, csp){
 
 }
 
-function orderDomainValues(course, possibleRooms, assignment, csp){
+function orderDomainValues(course, possibleRooms, assignment, csp) {
     return possibleRooms.slice().sort((a, b) => {
         return countConflicts(course, a, assignment, csp) - countConflicts(course, b, assignment, csp)
     })
 }
 
-function countConflicts(course, room, assignment, csp){
+function countConflicts(course, room, assignment, csp) {
 
     let count = 0
 
-    for(const neighbor of course.neighbors){
-        if (neighbor.name in assignment && assignment[neighbor] == room){
+    for (const neighbor of course.neighbors) {
+        if (neighbor.name in assignment && assignment[neighbor] == room) {
             count++
         }
     }
@@ -90,15 +107,57 @@ function countConflicts(course, room, assignment, csp){
     return count
 }
 
-function isConsistent(course, room, assignment, csp){
-    for (let neighbor in course.neighbors){
+function isConsistent(course, room, assignment, csp) {
+    for (let neighbor in course.neighbors) {
         let courseName = course.neighbors[neighbor].name
-        if ( courseName in assignment && assignment[courseName] == room){
+        if (courseName in assignment && assignment[courseName] == room) {
             return false
         }
     }
 
     return true
+}
+
+function arc_consistency(assignment, csp) {
+
+    queue = []
+
+    for (arc in csp.arcs) {
+        queue.push(csp.arcs[arc])
+    }
+
+    while (queue.length > 0) {
+        let arc = queue.shift()
+
+        if (revise(arc[0], arc[1], assignment, csp)) {
+            if (arc[0].possibleRooms.length == 0) {
+                return false
+            }
+
+            for (course in arc[0].neighbors) {
+                if (arc[0].neighbors[course] != arc[1]) {
+                    queue.push([arc[0].neighbors[course], arc[0]])
+                }
+            }
+        }
+    }
+
+    return true
+}
+
+function revise(courseA, courseB, assignment, csp) {
+
+    let revised = false
+
+    for (room in courseA.possibleRooms) {
+
+        if (!(courseB.possibleRooms.some(otherRoom => isConsistent(courseB, otherRoom, assignment, csp)))) {
+            courseA.possibleRooms.splice(room, 1)
+            revised = true
+        }
+    }
+
+    return revised
 }
 
 console.log(backtrackingSearch(CSPProblem))
