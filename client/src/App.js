8720bd6dc3c553d41260b091095a465fbf62626d
Abdoullatoundji <Abdoullatoundji@unomaha.edu>
2024-04-15 13:06:58 -0500

    made change to calendar.js as well

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@
  * @namespace RenderNavbar
  */
 
-import Navbar from "./Navbar"
-import Home from "./pages/Home"
-import Upload from "./pages/Upload"
-import Calendar from "./pages/Calendar"
-import AlgoResult from "./pages/AlgoResult"
-import { Route, Routes } from "react-router-dom"
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Calendar from "./pages/Calendar";
+import AlgoResult from "./pages/AlgoResult";
+import { Route, Routes } from "react-router-dom";
+import "./App.css"; // Import the App.css file for styling
+
 
 /**
  * Function for rendering the Navigation bar
@@ -18,9 +20,9 @@ import { Route, Routes } from "react-router-dom"
  */
 function App() {
     return (
-        <>
+        <div className="app-container">
             <Navbar />
-            <div className="container">
+            <div className="page-container">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/upload" element={<Upload />} />
@@ -28,8 +30,9 @@ function App() {
                     <Route path="/algorithm" element={<AlgoResult />} />
                 </Routes>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default App
+export default App;
+
--- client/src/pages/AlgoResult.js
+++ client/src/pages/AlgoResult.js
@@ -4,29 +4,44 @@
  * @namespace AlgorithmResults
  */
 
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import img from '../resources/O-UNO_Type_Color_White.png';
 import img2 from '../resources/photo-1606761568499-6d2451b23c66.avif';
+import "./AlgoResult.css";
 
-let response;//Response from axios GET request to /Algo
-let result;//Result of resolved response
-let algoResult = [];//Array for resolved data
-
-response = await axios.get("http://localhost:3001/Algo");//Store response from GET request to /Algo
-result = response.data;//Resolve data from response into result
-algoResult.push(JSON.stringify(result, null, 2));//Push data in string format to algoResult array
 
 /**
  * Function for displaying the raw data output from the algorithm
  * @returns {html} Html page containing the raw output data from the algorithm
  * @memberof AlgorithmResults
  */
-export default function AlgoResult(){
-    return <div>
-               <body style={{backgroundImage: `url(${img2})`, backgroundSize: "contain", backgroundRepeat: "repeat-y", bacbackgroundPosition: "top"}}>
-                <h1 style={{backgroundImage: `url(${img})`, backgroundSize: "cover", height: "110px", backgroundColor: "black"}}></h1>
-                <h2 style={{backgroundColor: "black", color: "white", width: "15%", margin: "auto", textAlign: "center"}}>Algorithm Results</h2>
-                <h3 style={{textAlign: "left", margin: "auto", backgroundColor: "black", color: "white", width: "70%"}}><pre>{algoResult}</pre></h3>
-                </body>
-            </div>
+export default function AlgoResult() {
+  const [algoResult, setAlgoResult] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/Algo");
+        setAlgoResult(JSON.stringify(response.data, null, 2));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="algo-container">
+      <header className="algo-header">
+        <img src={img} alt="Logo" className="algo-logo" />
+        <h1 className="algo-title">Algorithm Results</h1>
+      </header>
+      <div className="algo-content">
+        <p className="algo-data">{algoResult}</p>
+      </div>
+    </div>
+  );
 }
+
