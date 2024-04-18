54a97baa3cb9b127e762e8170c77e4a6e70e7563
Travis McMahon <travis.mcmahon@ssandtech.com>
2024-04-18 07:48:25 -0500

    Added all rooms to the calendar, 391A => 391

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -116,12 +116,36 @@ cData = JSON.parse(cData, function (key, value) {
 const currentDate = '2024-01-01';
 //Add all the classrooms
 const rooms = [
+  {text: 'Room 108', id: 108},
+  {text: 'Room 145', id: 145},
+  {text: 'Room 149', id: 149},
+  {text: 'Room 150', id: 150},
+  {text: 'Room 151', id: 151},
+  {text: 'Room 153', id: 153},
+  {text: 'Room 155', id: 155},
+  {text: 'Room 157', id: 157},
+  {text: 'Room 160', id: 160},
+  {text: 'Room 161', id: 161},
+  {text: 'Room 164', id: 164},
+  {text: 'Room 249', id: 249},
+  {text: 'Room 250', id: 250},
   {text: 'Room 252', id: 252},
   {text: 'Room 256', id: 256},
+  {text: 'Room 259', id: 259},
+  {text: 'Room 260', id: 260},
+  {text: 'Room 261', id: 261},
   {text: 'Room 263', id: 263},
+  {text: 'Room 269', id: 269},
+  {text: 'Room 270', id: 270},
+  {text: 'Room 274', id: 274},
   {text: 'Room 276', id: 276},
   {text: 'Room 278', id: 278},
-  {text: 'Room 279', id: 279}
+  {text: 'Room 279', id: 279},
+  {text: 'Room 350', id: 350},
+  {text: 'Room 352', id: 352},
+  {text: 'Room 361', id: 361},
+  {text: 'Room 368', id: 368},
+  {text: 'Room 391A', id: 391}
 ]
 
 export default class Demo extends React.PureComponent {
